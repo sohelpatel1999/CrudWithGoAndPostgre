e324d1c3f31e889679c5bff2b2d94541d4d96637
sohelpatel1999 <patelsohel1999@gmail.com>
2023-10-04 11:44:07 +0530

    [Sohel] Buided Connection with database sucessfully using connectionDatabase.

--- database/database.go
+++ database/database.go
@@ -1,14 +1,17 @@
-package main
+package database
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func ConnectionDatabase() (*gorm.DB, error) {
 
-	db, err := gorm.Open("postgre", "host=localhost port=5432 user=postgre dbname=admin sslmode=disable")
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=admin sslmode=disable")
 	if err != nil {
+		fmt.Println("error in db")
 		return nil, err
 	}
 	return db, nil
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"gowithpostgrecrud/database"
 )
 
 func main() {
 	fmt.Println("sohel bole")
+	db, err := database.ConnectionDatabase()
+	if err != nil {
+		panic("Error While Building Connection")
+	}
+	fmt.Println("connection build succesfully", db)
+	defer db.Close()
 }
